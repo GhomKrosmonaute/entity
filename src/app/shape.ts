1bd759a60cd0f09e03b6c0f00b0b31475a5a3ed8
CamilleAbella <camille.abella@hotmail.fr>
2022-02-14 20:17:03 +0100

    added some shapes

--- src/app/shape.ts
+++ src/app/shape.ts
@@ -85,3 +85,71 @@ export class Circle extends Shape {
     circle(this.x, this.y, this.diameter)
   }
 }
+
+export class Ellipse extends Rect {
+  get centerX() {
+    return this.x
+  }
+
+  get centerY() {
+    return this.y
+  }
+
+  get isHovered(): boolean {
+    return (
+      Math.pow(mouseX - this.x, 2) / Math.pow(this.width / 2, 2) +
+        Math.pow(mouseY - this.y, 2) / Math.pow(this.height / 2, 2) <=
+      1
+    )
+  }
+
+  onDraw() {
+    super.onDraw()
+    ellipse(this.x, this.y, this.width, this.height)
+  }
+}
+
+export class Line extends Shape {
+  constructor(
+    public x = 0,
+    public y = 0,
+    public x2 = 0,
+    public y2 = 0,
+    options?: DrawableSettings
+  ) {
+    super(options)
+  }
+
+  get width() {
+    return this.x2 - this.x
+  }
+
+  get height() {
+    return this.y2 - this.y
+  }
+
+  get size() {
+    return dist(this.x, this.y, this.x2, this.y2)
+  }
+
+  get centerX() {
+    return this.x + this.width / 2
+  }
+
+  get centerY() {
+    return this.y + this.height / 2
+  }
+
+  get isHovered(): boolean {
+    return (
+      dist(this.x, this.y, mouseX, mouseY) +
+        dist(mouseX, mouseY, this.x2, this.y2) <=
+      this.size
+    )
+  }
+
+  onDraw() {
+    super.onDraw()
+    line(this.x, this.y, this.x2, this.y2)
+  }
+}
