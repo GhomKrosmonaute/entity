613b95e11db47972799a26c77dafbfaf80668cf0
CamilleAbella <camille.abella@hotmail.fr>
2022-02-14 15:31:29 +0100

    extends Base entity

--- src/app/entity.ts
+++ src/app/entity.ts
@@ -1,75 +1,26 @@
-import { Entity as Base } from "@ghom/entity-base"
+import { Base } from "@ghom/entity-base"
 
 export type EntityEventName =
   | "setup"
-  | "draw"
   | "update"
   | "teardown"
+  | "draw"
   | "mousePressed"
   | "mouseReleased"
   | "keyPressed"
   | "keyReleased"
 
-export type EntityListener<This extends Entity> = (
-  this: This,
-  it: This
-) => unknown
-
 export class Entity extends Base<EntityEventName> {
-  protected _isSetup = false
   protected _zIndex?: number
-  protected _listeners: EntityListener<this>[] = []
-  protected _stopPoints: Record<EntityEventName, boolean> = {
-    setup: false,
-    draw: false,
-    update: false,
-    teardown: false,
-    keyPressed: false,
-    keyReleased: false,
-    mousePressed: false,
-    mouseReleased: false,
-  }
-
-  get isSetup() {
-    return this._isSetup
-  }
-
-  get children(): Array<Entity> {
-    return [...this._children]
-  }
 
   get zIndex(): number {
     return this._zIndex ?? this.parent?.children.indexOf(this) ?? 0
   }
 
-  get parent(): Entity | undefined {
-    return this._parent
-  }
-
-  /**
-   * Represent any state-based entity
-   */
-  constructor() {}
-
-  /**
-   * Used to be overwritten by your own workings
-   */
-  onSetup() {}
-
   /**
    * Used to be overwritten by your own workings
    */
   onDraw() {}
-
-  /**
-   * Used to be overwritten by your own workings
-   */
-  onUpdate() {}
-
-  /**
-   * Used to be overwritten by your own workings
-   */
-  onTeardown() {}
 
   /**
    * Used to be overwritten by your own workings
@@ -95,54 +46,12 @@ export class Entity extends Base<EntityEventName> {
    * Should only be called if the current entity is a root.
    * Should not be overwritten!
    */
-  public setup() {
-    if (!this.isSetup) {
-      this.onSetup()
-      this.transmit("setup")
-      this._isSetup = true
-    } else {
-      throw new Error("Entity is already setup")
-    }
-  }
-
-  /**
-   * Should only be called if the current entity is a root.
-   * Should not be overwritten!
-   */
   public draw() {
     if (this.isSetup) {
       this.onDraw()
       this.transmit("draw")
     } else {
       console.warn("Draw is called before setup")
-    }
-  }
-
-  /**
-   * Should only be called if the current entity is a root.
-   * Should not be overwritten!
-   */
-  public update() {
-    if (this.isSetup) {
-      this.onUpdate()
-      this.transmit("update")
-    } else {
-      console.warn("update is called before setup")
-    }
-  }
-
-  /**
-   * Should only be called if the current entity is a root.
-   * Should not be overwritten!
-   */
-  public teardown() {
-    if (this.isSetup) {
-      this._isSetup = false
-      this.onTeardown()
-      this._parent?.removeChild(this)
-      this.transmit("teardown")
-    } else {
-      throw new Error("Entity must be setup before")
     }
   }
 
@@ -198,36 +107,7 @@ export class Entity extends Base<EntityEventName> {
     }
   }
 
-  public on(name: EntityEventName, listener: EntityListener<this>) {
-    this._listeners.push(
-      {
-        [name]() {
-          listener.bind(this)(this)
-        },
-      }[name].bind(this)
-    )
-  }
-
-  public addChild(...children: Entity[]) {
-    for (const child of children) {
-      child._parent = this
-      this._children.add(child)
-      if (this.isSetup) child.setup()
-    }
-  }
-
-  public removeChild(...children: Entity[]) {
-    for (const child of children) {
-      if (child.isSetup) child.teardown()
-      else this._children.delete(child)
-    }
-  }
-
-  public stopTransmission(name: EntityEventName) {
-    this._stopPoints[name] = true
-  }
-
-  private transmit(name: EntityEventName) {
+  protected transmit(name: EntityEventName) {
     for (const listener of this.getListenersByName(name))
       listener.bind(this)(this)
 
@@ -247,29 +127,5 @@ export class Entity extends Base<EntityEventName> {
 
       child[name]()
     }
-  }
-
-  private getListenersByName(name: EntityEventName) {
-    return this._listeners.filter((listener) => {
-      return listener.name === name
-    })
-  }
-
-  public schema(
-    indentation = 2,
-    depth = 0,
-    index: number | null = null
-  ): string {
-    return `${" ".repeat(indentation).repeat(depth)}${
-      index === null ? "" : `${index} - `
-    }${this.constructor.name} [${this.isSetup ? "on" : "off"}]${
-      this._children.size > 0
-        ? `:\n${this.children
-            .map(
-              (child, index) => `${child.schema(indentation, depth + 1, index)}`
-            )
-            .join("\n")}`
-        : ""
-    }`
   }
 }
